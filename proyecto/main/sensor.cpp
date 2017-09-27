3305bcf4fbc6a770fb9c714b1a646ec11ae80b1e
agusss.garcia19 <agusss.garcia19@gmail.com>
2017-09-27 20:03:23 -0300

    Cambio en el scan del Sensor

--- proyecto/main/sensor.cpp
+++ proyecto/main/sensor.cpp
@@ -9,12 +9,9 @@ Sensor::Sensor(const unsigned char sensor_pin, const unsigned char default_value
 
 void Sensor::scan()
 {
-	while (1)
-	{
-		current_state = digitalRead(sensor_pin);
-		if (current_state != previous_state)
-			previous_state = current_state;
-	}
+	current_state = digitalRead(sensor_pin);
+	if (current_state != previous_state)
+		previous_state = current_state;
 }
 
 unsigned char Sensor::get_current_state()
