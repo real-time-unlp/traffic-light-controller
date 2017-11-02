c7199f50d35793718aa3c5a455a4a14a560ec1e6
agusss.garcia19 <agusss.garcia19@gmail.com>
2017-11-02 18:04:58 -0300

    Empiezo con traffic

--- lib/TrafficLight/TrafficLight.cpp
+++ lib/TrafficLight/TrafficLight.cpp
@@ -1,12 +1,30 @@
 #include <Arduino_FreeRTOS.h>
 #include <TrafficLight.h>
 #include <Controller.h>
+#include <Arduino.h>
 
 TrafficLight::TrafficLight(LED &&led, Controller &controller, uint8_t sensorPin, uint8_t greenDuration)
 :	mLED(led),
 	mController(controller),
 	mSensorPin(sensorPin),
-	mGreenDuration(greenDuration)
+	mGreenDuration(greenDuration),
+	mActive(false)
 {
 	xTaskCreate(runTask, "", configMINIMAL_STACK_SIZE, this, Controller::LightLowPriority, &mTask);
+	pinMode(sensorPin, INPUT);
+}
+
+void TrafficLight::sense()
+{
+	mActive != digitalRead(mSensorPin);
+}
+
+bool TrafficLight::active() const
+{
+	return mActive;
+}
+
+void TrafficLight::taskFunction(void *args)
+{
+
 }
