d7730bc0fe0c4f9cbad7195c7a422aa4e69cf095
agusss.garcia19 <agusss.garcia19@gmail.com>
2017-11-02 17:26:23 -0300

    Agrego RTOSCOnfig original

--- lib/FreeRTOSConfig/FreeRTOSConfig.h
+++ lib/FreeRTOSConfig/FreeRTOSConfig.h
@@ -90,15 +90,15 @@
 #define configUSE_TICK_HOOK                 0
 #define configCPU_CLOCK_HZ                  ( ( uint32_t ) F_CPU )			// This F_CPU variable set by the environment
 #define configMAX_PRIORITIES                ( ( UBaseType_t ) 4 )
-#define configMINIMAL_STACK_SIZE            ( ( UBaseType_t ) 64 )
-#define configIDLE_STACK_SIZE               ( ( UBaseType_t ) 64 )
-#define configMAX_TASK_NAME_LEN             ( 2 )
+#define configMINIMAL_STACK_SIZE            ( ( UBaseType_t ) 85 )
+#define configIDLE_STACK_SIZE               ( ( UBaseType_t ) 192 )
+#define configMAX_TASK_NAME_LEN             ( 8 )
 #define configUSE_TRACE_FACILITY            0
 #define configUSE_16_BIT_TICKS              1
 #define configIDLE_SHOULD_YIELD             1
 
-#define configUSE_MUTEXES                   0
-#define configUSE_RECURSIVE_MUTEXES         0
+#define configUSE_MUTEXES                   1
+#define configUSE_RECURSIVE_MUTEXES         1
 #define configUSE_COUNTING_SEMAPHORES       1
 #define configUSE_QUEUE_SETS                0
 #define configQUEUE_REGISTRY_SIZE           0
@@ -110,7 +110,7 @@
 
 
 /* Timer definitions. */
-#define configUSE_TIMERS                    0
+#define configUSE_TIMERS                    1
 #define configTIMER_TASK_PRIORITY           ( ( UBaseType_t ) 3 )
 #define configTIMER_QUEUE_LENGTH            ( ( UBaseType_t ) 10 )
 #define configTIMER_TASK_STACK_DEPTH        configMINIMAL_STACK_SIZE
@@ -125,17 +125,17 @@
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet                0
-#define INCLUDE_uxTaskPriorityGet               0
-#define INCLUDE_vTaskDelete                     0
+#define INCLUDE_vTaskPrioritySet                1
+#define INCLUDE_uxTaskPriorityGet               1
+#define INCLUDE_vTaskDelete                     1
 #define INCLUDE_vTaskCleanUpResources           0
 #define INCLUDE_vTaskSuspend                    1
-#define INCLUDE_vResumeFromISR                  0
-#define INCLUDE_vTaskDelayUntil                 0
+#define INCLUDE_vResumeFromISR                  1
+#define INCLUDE_vTaskDelayUntil                 1
 #define INCLUDE_vTaskDelay                      1
 #define INCLUDE_xTaskGetSchedulerState          0
 #define INCLUDE_xTaskGetIdleTaskHandle          0 // create an idle task handle.
 #define INCLUDE_xTaskGetCurrentTaskHandle       0
-#define INCLUDE_uxTaskGetStackHighWaterMark     0
+#define INCLUDE_uxTaskGetStackHighWaterMark     1
 
 #endif /* FREERTOS_CONFIG_H */
